9310d61149d5ef1101b23dc835914ea41805c216
Itoro Emmanuel Philip <56868676+itorophilip1998@users.noreply.github.com>
2022-03-13 11:04:27 +0100

    Dev
    Dev

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,5 +2,8 @@ export default function ({ store, redirect }) {
   const authorized=localStorage.getItem('token') 
   if (!authorized || authorized==='undefined') {
     return redirect('/login')
+  }  else{
+
+return redirect('/chat')
   }  
 }
