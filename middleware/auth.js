aad4892fc0c8b21739e3be099d1e9449edd834f4
Itoro Emmanuel Philip <56868676+itorophilip1998@users.noreply.github.com>
2022-03-13 11:12:59 +0100

    Chat
    Dev

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,7 @@ export default function ({ store, redirect }) {
   const authorized=localStorage.getItem('token') 
   if (!authorized || authorized==='undefined') {
     return redirect('/login')
-  }  else{
-
-return redirect('/chat')
   }  
+  
+   
 }
