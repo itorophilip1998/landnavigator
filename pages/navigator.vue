9c41591be40b9f317a4e45e53e214e2284bea148
itphilip1998 <itkeyzzpianny@gmail.com>
2022-04-16 16:44:04 +0100

    all

--- pages/navigator.vue
+++ pages/navigator.vue
@@ -1,13 +1,25 @@
 <template>
-  <div class="vh-100"> 
-      <Loading v-if="loading"/>
-      
-  </div>
+  <div>
+    <div class="text-center p-2 pt-3">
+      <h6>Go everywhere</h6>
+      <GmapAutocomplete @place_changed="setPlace" class="form-control" />
+      <button @click="addMarker" class="btn btn-primary locate">
+        <i class="fa fa-map-marker" aria-hidden="true"></i>
+      </button>
+    </div>
+    <br />
+    <GmapMap :center="center" :zoom="12" style="width: 100%; height: 400px">
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        @click="center = m.position"
+      />
+    </GmapMap>
   </div>
 </template>
 
-
-<script>  
+<script>
 import Vue from 'vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
 Vue.use(VueGoogleMaps, {
@@ -15,32 +27,52 @@ Vue.use(VueGoogleMaps, {
     key: 'YOUR_GOOGLE_MAPS_API_KEY_GOES_HERE',
     libraries: 'places',
   },
-});
+})
 export default {
-    name: 'GoogleMap',
-
-    components:{
-        Loading
-    }, 
-  
-  
+  name: 'GoogleMap',
   data() {
     return {
-    
-   
+      center: { lat: 45.508, lng: -73.587 },
+      currentPlace: null,
+      markers: [],
+      places: [],
     }
   },
-
-  mounted(){ 
+  mounted() {
+    this.geolocate()
   },
-   
   methods: {
-         
-  }
-
+    setPlace(place) {
+      this.currentPlace = place
+    },
+    addMarker() {
+      if (this.currentPlace) {
+        const marker = {
+          lat: this.currentPlace.geometry.location.lat(),
+          lng: this.currentPlace.geometry.location.lng(),
+        }
+        this.markers.push({ position: marker })
+        this.places.push(this.currentPlace)
+        this.center = marker
+        this.currentPlace = null
+      }
+    },
+    geolocate: function () {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        }
+      })
+    },
+  },
 }
 </script>
-<style > 
 
+<style>
+.locate {
+  position: absolute;
+  top: 43px;
+  right: 0px;
+}
 </style>
-
